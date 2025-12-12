96ccf53d9f664e7f1fa87c165743b971c4db1608
Diolutviandr3 <diolutviandre1412@gmail.com>
2025-12-12 07:34:14 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Ruang_Ekspresi-Dio_Lutvi/',
   plugins: [react()],
   server: {
     port: 3000, // Opsional: Mengatur port default ke 3000
